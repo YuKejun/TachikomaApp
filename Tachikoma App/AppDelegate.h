98bd18f981272e864c357cb61916ee0aad0698dd
YuKejun <yukejun@umich.edu>
2015-07-03 14:35:37 +0800

    Initial Commit

---
+++ Tachikoma App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tachikoma App
+//
+//  Created by YuKejun on 7/3/15.
+//  Copyright (c) 2015 YuKejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tachikoma App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tachikoma App
+//
+//  Created by YuKejun on 7/3/15.
+//  Copyright (c) 2015 YuKejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
